253c443d4ff9239c67b561389184c2663d0a8bea
nares <nares@192.168.0.105>
2021-07-10 20:27:51 +0530

    added selenium hub and node changes

--- src/main/java/utilities/PageObject.java
+++ src/main/java/utilities/PageObject.java
@@ -8,22 +8,39 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class PageObject {
 	public static WebDriver driver;
 	public static Properties prop=new Properties();
 	public static FileInputStream objfile;
+	public static String nodeUrl = "http://192.168.0.102:5566/wd/hub";
 	@BeforeClass
 	public static void startUp() throws Exception {
-		
+		/*
+		 * This peace of  commented code is for executing the test cases on nodes
+		 * 
+		 * 
+		 */
+		/*DesiredCapabilities capibilities = DesiredCapabilities.firefox();
+		capibilities.setBrowserName("firefox");
+		capibilities.setVersion("89.0.1");
+		capibilities.setPlatform(Platform.LINUX);
+		capibilities.setCapability("marionette", true);
+		driver = new RemoteWebDriver(new URL(nodeUrl),capibilities);*/
 		objfile = new FileInputStream(System.getProperty("user.dir")+"\\application.properties");
 		prop.load(objfile);
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
+		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
